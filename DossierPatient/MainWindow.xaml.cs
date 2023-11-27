8780d3a4acdb2c9973639d1d948412d7b3ba4a85
Boris <hobitts2011@gmail.com>
2023-11-26 23:17:54 -0500

    main.cs

--- DossierPatient/MainWindow.xaml.cs
+++ DossierPatient/MainWindow.xaml.cs
@@ -19,5 +19,69 @@ namespace DossierPatient
   /// </summary>
     public partial class MainWindow : Window
     {
+        DatabasePatientEntities1 patientEntities;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //patientEntities = (DatabasePatientEntities)DataContext;
+            patientEntities = new DatabasePatientEntities1();
+            cboPatient.DataContext = patientEntities.Patients.ToList();
+
+        }
+
+
+
+
+
+        private void cboPatient_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lsDossierMedicals.DataContext = ((Patient)cboPatient.SelectedItem).DossierMedicals;
+        }
+
+        private void btnAjouterPatient_Click(object sender, RoutedEventArgs e)
+        {
+            DossierMedical dossier1 = new DossierMedical();
+            dossier1.IdPatient = 33333;
+            dossier1.Diagnostique = "ulcer gastrique";
+            dossier1.IdDossierMedical = 45892;
+            dossier1.Etat = "actife";
+
+            patientEntities.DossierMedicals.Add(dossier1);
+            patientEntities.SaveChanges();
+        }
+
+        private void btnSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            DossierMedical dossier1 = ((DossierMedical)lsDossierMedicals.SelectedItem);
+            patientEntities.DossierMedicals.Remove(dossier1);
+            patientEntities.SaveChanges();
+        }
+
+        private void btnDiagnostiqueActive_Click(object sender, RoutedEventArgs e)
+        {
+            DossierMedical dossier1 = (DossierMedical)lsDossierMedicals.SelectedItem;
+            dossier1.Etat = "Active";
+            patientEntities.SaveChanges();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (checkBox.IsChecked == true)
+            {
+                List<DossierMedical> dossiersActives = (from c in ((Patient)cboPatient.SelectedItem).
+                                                        DossierMedicals
+                                                        where c.Etat == "active"
+                                                        select c).ToList();
+                lsDossierMedicals.DataContext = dossiersActives;
+            }
+            else
+            {
+                lsDossierMedicals = (ListBox)((Patient)cboPatient.SelectedItem).DossierMedicals;
+            }
+        }
     }
 }
